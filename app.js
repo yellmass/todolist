7a692c0f1a3ae06958a4e3b4db07c3e82f34bab8
yellmass <ylmzbnc@users.noreply.github.com>
2023-03-07 16:18:28 -0500

    version 2, ready to deploy to cyclic

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB")
+mongoose.connect("mongodb+srv://admin-yellmass:test123@cluster0.jmdnsn8.mongodb.net/todoListDB")
 
 const itemsSchema={
   name:String
@@ -122,6 +122,6 @@ app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Server started on port 3000");
 });
